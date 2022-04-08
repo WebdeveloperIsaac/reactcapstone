73b2714ad80f3459ef1d7af1798aae47e4f659e7
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-08 20:58:59 +0530

    Click event with a card and aseperate dish file and also the other thing is dishselect and update and all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
+import { Component } from 'react';
 import { Navbar, NavbarBrand  } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      dishes: DISHES
+    };
+  }
+  render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,9 +21,10 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu></Menu>
+      <Menu dishes={this.state.dishes} ></Menu>
     </div>
   );
+}
 }
 
 export default App;
