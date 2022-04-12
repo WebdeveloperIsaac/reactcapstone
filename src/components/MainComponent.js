4a7fa51b2ad5fd0dc11c88886d5dedddc71d3a27
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-12 16:10:19 +0530

    Created the Home Page with some dta filtering the json data and all in the format of a card

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,10 @@ import { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import Contact from'./ContactComponent';
 import Header from './HeaderComponent'
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
@@ -13,6 +17,9 @@ class Main extends Component {
 
     this.state={
       dishes: DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     };
 
 }
@@ -21,7 +28,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
   return (
@@ -29,8 +39,9 @@ class Main extends Component {
     <div>
       <Header/>
       <Switch>
-        <Route path="/home" component={Home}/>
+        <Route path="/home" component={HomePage}/>
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+        <Route exact path="/contactus" component={Contact}/>
         <Redirect to='/home'></Redirect>
       </Switch>
       <Footer/>
