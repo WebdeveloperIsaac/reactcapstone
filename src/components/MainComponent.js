9bbbecd8827b56e12df18d5af96ec90b091ae189
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-12 17:53:10 +0530

    added the functionality of loading the comments and all the things routing and stuff

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,53 +1,61 @@
-
-import { Component } from 'react';
+import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-import Contact from'./ContactComponent';
-import Header from './HeaderComponent'
-import DishDetail from './DishdetailComponent';
-import Footer from './FooterComponent';
-import {Switch,Route,Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 class Main extends Component {
+
   constructor(props) {
     super(props);
-
-    this.state={
+    this.state = {
       dishes: DISHES,
-      comments:COMMENTS,
-      promotions:PROMOTIONS,
-      leaders:LEADERS
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
+  }
 
-}
-
-  render(){
+  render() {
 
     const HomePage = () => {
-      return(
+      return (
         <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-        />
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
-  return (
 
-    <div>
-      <Header/>
-      <Switch>
-        <Route path="/home" component={HomePage}/>
-        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
-        <Route exact path="/contactus" component={Contact}/>
-        <Redirect to='/home'></Redirect>
-      </Switch>
-      <Footer/>
-    </div>
-  );
-}
+    const DishWithId = ( {match} ) => {
+      return (
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+      );
+    }
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          { /* If "exact" is not set, "/menu/xy" will match */ }
+          <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+
 }
 
 export default Main;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay,CardTitle  } from "reactstrap";
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderMenuItem({ dish,onClick }){
-return(
-    <Card onClick={() =>onClick(dish.id)} >
-
-                        <CardImg width="100%" src={ dish.image } alt={ dish.name } />
-                        <CardImgOverlay>
-                            <CardTitle> { dish.name }</CardTitle>
-                        </CardImgOverlay>
-                    </Card>   
-);
-}
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
+            </Card>
+        );
+    }
+    
 
 
    const Menu =(props) => {
@@ -27,7 +31,17 @@ return(
         return(
             <div className="container">
                 <div className="row">
-                    { menu }
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {menu}
                 </div>
             </div>
         );
