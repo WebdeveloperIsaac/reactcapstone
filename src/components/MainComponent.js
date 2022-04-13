6dfdd3dd04791751b5df863908881200839fc49b
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-13 23:25:14 +0530

    added the controlled form and did the validation procedure for it

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -49,6 +50,7 @@ class Main extends Component {
           <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={ ()=> <About leaders={this.state.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
