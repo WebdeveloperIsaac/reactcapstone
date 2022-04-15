0b5d1facc6df56b9e410cba56e3dd17cd2259dc0
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-15 20:14:55 +0530

    implemented the main basic redux with the reducer asnd configure store for the states with the suitable methods for implementation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 class App extends Component {
+
   render(){
   return (
-    <BrowserRouter>
-    <div >
-      <Main></Main>
-    </div>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+
   );
 }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,63 +1,99 @@
 import React, { Component } from 'react';
+
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Contact from './ContactComponent';
-import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
-import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+
+
+const mapStateToProps = state => {
+
+    return{
+        comments: state.comments,
+        dishes: state.dishes,
+        leaders: state.leaders,
+        promotions: state.promotions,
+    }
+}       
+
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      leaders: LEADERS,
-      promotions: PROMOTIONS
-    };
-  }
-
-  render() {
-
-    const HomePage = () => {
-      return (
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
-      );
+    constructor(props) {
+        super(props);
     }
 
-    const DishWithId = ( {match} ) => {
-      return (
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
-      );
-    }
+    render() {
 
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/home" component={HomePage} />
-          { /* If "exact" is not set, "/menu/xy" will match */ }
-          <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
-          <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component={Contact} />
-          <Route exact path="/aboutus" component={ ()=> <About leaders={this.state.leaders}/>} />
-          <Redirect to="/home" />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={ this.props.dishes.filter( (dish)=>dish.featured )[0] }
+                    promotion={this.props.promotions.filter( (promotion)=>promotion.featured )[0] }
+                    leader={this.props.leaders.filter( (leader)=>leader.featured )[0] }
+                />
+            );
+        };
+
+        const AboutUsPage = () => {
+            return(
+                <About 
+                    leaders={this.props.leaders}
+                />
+            );
+        };
+
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail 
+                
+                dish={this.props.dishes.filter( (dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+                comments={this.props.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) } 
+                
+                
+                />
+            );
+        };
+
+
+
+        return (
+            <div>
+                <Header></Header>
+
+                <Switch>
+                    <Route path="/home" component={ HomePage } />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/> }/>
+
+                    <Route path="/menu/:dishId" component={DishWithId} />
+
+                    <Route exact path="/contactus" component={Contact } />
+                    <Route exact path="/aboutus" component={ AboutUsPage } />
+                   
+                    {/* if url dosesnt match, bydefault redirect to */}
+                    <Redirect to="/home" />
+                </Switch>
+
+                <Footer></Footer>
+            </div> 
+        );
+
+    }
 
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
+
+
+/**
+ * 
+ * - connect(): generates a wrapper container component that 
+ *      subscribe to the store.
+ */
