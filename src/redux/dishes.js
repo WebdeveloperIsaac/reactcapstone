3cf86f2998914b755d585d5e4304b1fb17416b24
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-04-17 22:01:21 +0530

    added and splitted the reducers that are needed to be done

---
+++ src/redux/dishes.js
@@ -0,0 +1,9 @@
+import { DISHES } from '../shared/dishes';
+
+export const Dishes = (state = DISHES,action) => {
+    switch (action.type) 
+    {
+        default :
+                return state;
+    }
+}
---
+++ src/redux/leaders.js
@@ -0,0 +1,10 @@
+import { LEADERS } from '../shared/leaders';
+
+
+export const Leaders = (state = LEADERS,action) => {
+    switch (action.type) 
+    {
+        default :
+                return state;
+    }
+}
